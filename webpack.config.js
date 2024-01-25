a40c4e2dc322d98ee9fb4b68558f92d1d72b9057
Chris <chris79gp@outlook.com>
2024-01-25 15:31:02 +0000

    clean-up in preparation for output management tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,4 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -14,55 +10,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
-            title: 'Webpack 5 HTML Plugin',
             filename: 'index.html',
             inject: 'head',
             scriptLoading: 'defer',
         })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ['csv-loader'],
-            },
-            {
-                test: /\.xml$/i,
-                use: ['xml-loader'],
-            },
-            {
-                test: /\.toml$/i,
-                type: 'json',
-                parser: {
-                    parse: toml.parse,
-                },
-              },
-              {
-                test: /\.yaml$/i,
-                type: 'json',
-                parser: {
-                    parse: yaml.parse,
-                },
-              },
-              {
-                test: /\.json5$/i,
-                type: 'json',
-                parser: {
-                    parse: json5.parse,
-                },
-            },
-        ],
-    },
+    ]
 };
